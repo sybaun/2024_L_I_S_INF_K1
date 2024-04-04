b1a0a258ca3ac3c7d58996f6324e51c6d20d351c
vdi-terminal <vdi-terminal@EDU-WSB.local>
2024-04-04 09:07:03 +0200

    błąd 2

--- test/test_views.py
+++ test/test_views.py
@@ -8,11 +8,11 @@ class FlaskrTestCase(unittest.TestCase):
         app.config['TESTING'] = True
         self.app = app.test_client()
 
-    def test_outputs(self):
+    def tet_outputs(self):
         rv = self.app.get('/outputs')
         s = str(rv.data)
         ','.join(SUPPORTED) in s
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEqual(b'{ "imie":"Alber", "mgs":Hello World!"}', rv.data)
+        self.assertEqual(b'{ "imie":"Albert", "mgs":Hello World!"}', rv.data)
